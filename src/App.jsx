f1fa3c19f1fd0687e1964422044bbaa04c03c6fd
quangkid2212 <163752892+hckq2212@users.noreply.github.com>
2025-11-06 15:21:50 +0700

    Sửa sidemenu

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,29 @@ import Project from "./screens/Project";
 import Jobs from "./screens/Jobs";
 import { store } from './app/store'
 import { Provider } from 'react-redux';
-
+import CreateOpportunity from "./components/Opportunity/CreateOpportunity";
+import SideMenu from "./components/ui/SideMenu";
+import Header from "./components/ui/Header";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Provider store = {store}>
-        <Routes>
+        <Header />
+        <SideMenu />
+        <div className="ml-[17%] mt-[100px]">
+          <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/opportunity" element={<Opportunity />} />
+          <Route path="/opportunity/create" element={<CreateOpportunity />} />
           <Route path="/contract" element={<Contracts />} />
           <Route path="/contracts/hr" element={<ContractsHR />} />
           <Route path="/project" element={<Project />} />
           <Route path="/job" element={<Jobs />} />
         </Routes>
+        </div>
       </Provider>
     </BrowserRouter>
   );
--- src/components/ui/SideMenu.jsx
+++ src/components/ui/SideMenu.jsx
@@ -1,17 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa"; // Sử dụng icon từ react-icons
 
 export default function SideMenu () {
+    const [opportunityList, setOpportunityList] = useState(false);
+    const [contractList, setContractList] = useState(false);
+    const [jobList, setJobList] = useState(false);
+    const [projectList, setProjectList] = useState(false);
+
+    const toggleOpportunityList = () => {
+        setOpportunityList(prevState => !prevState);
+    };
+    const toggleContractList = () => {
+        setContractList(prevState => !prevState);
+    };
+    const toggleJobList = () => {
+        setJobList(prevState => !prevState);
+    };
+    const toggleProjectList = () => {
+        setProjectList(prevState => !prevState);
+    };
+
     return (
-        <div
-            className="fixed left-0 w-[14%] bg-[#e7f1fd] top-[80px] h-[100%]"
-        >
-           <ul className="flex flex-col text-left">
-                <li className=" p-4 "><a href="/" className="text-[#184172] text-xl font-semibold">Bảng điều khiển</a></li>
-                <li className=" p-4 "><a href="/opportunity" className="text-[#184172] text-xl font-semibold">Cơ hội</a></li>
-                <li className=" p-4 "><a href="/contract"  className="text-[#184172] text-xl font-semibold">Hợp đồng</a></li>
-                <li className=" p-4 "><a href="/project"  className="text-[#184172] text-xl font-semibold">Dự án</a></li>
-                <li className=" p-4 "><a href="/job"  className="text-[#184172] text-xl font-semibold">Công việc</a></li>
-           </ul>
+        <div className="fixed left-0 w-[250px] bg-[#e7f1fd] top-[80px] h-[100%] shadow-md">
+            <ul className="flex flex-col text-left p-4">
+                <li className="p-4">
+                    <a href="/" className="text-[#184172] text-lg font-semibold">Bảng điều khiển</a>
+                </li>
+                
+                {/* Cơ hội */}
+                <li className="p-4 cursor-pointer">
+                    <div onClick={toggleOpportunityList} className="flex justify-between items-center">
+                        <span className="text-[#184172] text-md font-semibold">Cơ hội</span>
+                        {opportunityList ? <FaChevronUp /> : <FaChevronDown />}
+                    </div>
+                    {opportunityList && (
+                        <ul className="p-2 flex flex-col gap-2">
+                            <li><a href="/opportunity/create" className="text-[#184172]">Tạo cơ hội</a></li>
+                            <li><a href="/opportunity" className="text-[#184172]">Danh sách cơ hội</a></li>
+                        </ul>
+                    )}
+                </li>
+
+                {/* Công việc */}
+                <li className="p-4 cursor-pointer">
+                    <div onClick={toggleJobList} className="flex justify-between items-center">
+                        <span className="text-[#184172] text-md font-semibold">Công việc</span>
+                        {jobList ? <FaChevronUp /> : <FaChevronDown />}
+                    </div>
+                    {jobList && (
+                        <ul className="p-2 flex flex-col gap-2 ">
+                            <li><a href="/jobs" className="text-[#184172]">Danh sách công việc</a></li>
+                        </ul>
+                    )}
+                </li>
+
+                {/* Khách hàng */}
+                <li className="p-4 cursor-pointer">
+                    <div onClick={toggleContractList} className="flex justify-between items-center">
+                        <span className="text-[#184172] text-md font-semibold">Khách hàng</span>
+                        {contractList ? <FaChevronUp /> : <FaChevronDown />}
+                    </div>
+                    {contractList && (
+                        <ul className="p-2 flex flex-col gap-2">
+                            <li><a href="/customers" className="text-[#184172]">Danh sách khách hàng</a></li>
+                        </ul>
+                    )}
+                </li>
+
+
+                {/* Chương trình */}
+                <li className="p-4 cursor-pointer">
+                    <div onClick={toggleProjectList} className="flex justify-between items-center">
+                        <span className="text-[#184172] text-md font-semibold">Chương trình</span>
+                        {projectList ? <FaChevronUp /> : <FaChevronDown />}
+                    </div>
+                    {projectList && (
+                        <ul className="p-2 flex flex-col gap-2">
+                            <li><a href="/projects" className="text-[#184172]">Danh sách chương trình</a></li>
+                        </ul>
+                    )}
+                </li>
+            </ul>
         </div>
-    )
+    );
 }
